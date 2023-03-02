47ae9377f8ed6048fa89c2c6da5b6efacf4ea9bc
Cualone <june568@naver.com>
2023-03-02 09:29:14 +0900

    first

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명령) ");
+        String com = sc.next();
+        if (com=="등록") {
+            System.out.println("명언 : ");
+            String myeong = sc.next();
+            System.out.println("작가 : ");
+            String by = sc.next();
+        }
     }
 }
