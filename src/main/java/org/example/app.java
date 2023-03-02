c23190467ede3032a55e0a364669ceca932d4c2a
Cualone <june568@naver.com>
2023-03-02 11:14:13 +0900

    fifth-1

--- src/main/java/org/example/app.java
+++ src/main/java/org/example/app.java
@@ -33,7 +33,13 @@ public class app {
                 System.out.printf("%d번 명언이 등록되었습니다.\n", id);
                 lastWiseSayingId++;
             } else if (com.equals("목록")) {
-                System.out.printf("생성 명언 수 : %d\n", wiseSayings.size());
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("-".repeat(30));
+                for (int i=wiseSayings.size()-1;i>=0;i--) {
+                    WiseSaying wiseSaying = wiseSayings.get(i);
+                    System.out.printf("%d / %s / %s\n",wiseSaying.getId(),wiseSaying.getName(),wiseSaying.getContent());
+
+                }
             }
         }
     }
